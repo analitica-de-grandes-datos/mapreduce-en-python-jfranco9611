664e416fed96b6a019b043e4e3aaebde30424662
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-12 18:12:25 -0500

    PR3

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,5 +2,5 @@ import sys
 
 if __name__ == "__main__":
     for line in sys.stdin:
-        for word in line.split(','):
+        word = line.split(',')
         sys.stdout.write("{}\t1\n".format(word[2]))
