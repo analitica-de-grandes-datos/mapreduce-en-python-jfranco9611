29f826dbf9d20f9c8d46b0ef5c25f15d701be960
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-12 18:55:53 -0500

    pr3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -6,4 +6,4 @@ import sys
 if __name__ == "__main__":
     for line in sys.stdin:
         word = line.split(',')
-        sys.stdout.write("{}\t{}\n".format(word[1].strip(), word[0]))
+        sys.stdout.write("{}{}\n".format(word[1].strip(), word[0]))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -3,4 +3,4 @@ import sys
 if __name__ == "__main__":
     for line in sys.stdin:
         word = line.split(',')
-        sys.stdout.write("{}\t{}\n".format(word[1].strip(), word[0]))
+        sys.stdout.write("{},{}\n".format(word[1].strip(), word[0]))
