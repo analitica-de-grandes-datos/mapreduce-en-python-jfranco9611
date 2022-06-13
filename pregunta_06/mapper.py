90e33942a72b6aad21b8fcbd1836311e170b5ccb
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-12 19:39:37 -0500

    pr1

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,6 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        word = line.split(' ')
+        sys.stdout.write("{},{}\n".format(str(word[0]), float(word[2])))
