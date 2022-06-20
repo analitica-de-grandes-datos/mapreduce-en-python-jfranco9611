1c53c818e1de14207bce711ac1eed6eb0d87df96
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-20 13:21:29 -0500

    pr1

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,9 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+maxi = 2
+if __name__ == "__main__":
+    for line in sys.stdin:
+        a, b = line.split('\t')
+        a = a.zfill(maxi)
+        b =list(b.strip().split(','))
+        for x in b:
+            sys.stdout.write("{}\t{}\n".format(x,a))
