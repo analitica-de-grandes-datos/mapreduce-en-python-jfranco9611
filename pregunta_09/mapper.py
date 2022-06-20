b694b1fbd16cd86f1101ec79429ff73196552337
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-20 13:21:05 -0500

    Add files via upload

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,7 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+maxi = 3
+if __name__ == "__main__":
+    for line in sys.stdin:
+        strnum = line.split('   ')[2].strip()
+        strnum = strnum.zfill(maxi)
+        sys.stdout.write("{}   {}   {}\n".format(strnum, line.split('   ')[0] , line.split('   ')[1]))
