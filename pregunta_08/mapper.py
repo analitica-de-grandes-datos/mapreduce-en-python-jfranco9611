3b2b98d7b8ef27c515b8cced399775807d56707e
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-06-20 13:20:45 -0500

    pr1

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,5 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+maxi = 3
+if __name__ == "__main__":
+    for line in sys.stdin:
+        sys.stdout.write("{},{},1\n".format(str(line.split('   ')[0]), line.split('   ')[2].strip()))
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,26 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+    reg = 0
+    for line in sys.stdin:
+        key, val, count = line.split(",")
+        val = float(val)
+        count = float(count)
+
+        if key == curkey:
+            total += val
+            reg += count
+
+        else:
+            if curkey is not None:
+                prom = total / reg
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, total, prom))
+            curkey = key
+            total = val
+            reg = count
+
+    prom = total / reg
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, total, prom))
